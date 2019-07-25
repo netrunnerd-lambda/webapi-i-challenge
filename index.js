b4ff426375e46d74037b3103f8e2a56d181e1f5a
netrunnerd <41193007+netrunnerd@users.noreply.github.com>
2019-07-24 23:15:47 -0400

    PUT /api/users/:id

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ server.get('/api/users', (req, res) => {
   db
     .find()
     .then(users => res.status(200).json({ success: true, users }))
-    .catch(err => res.status(500).json({ success: false, error: "`Users` information could not be retrieved." }));
+    .catch(err => res.status(500).json({ success: false, error: "Users information could not be retrieved." }));
 });
 
 server.get('/api/users/:id', (req, res) => {
@@ -28,7 +28,7 @@ server.get('/api/users/:id', (req, res) => {
       if (user) res.status(200).json({ success: true, user });
       else res.status(404).json({ success: false, error: "User does not exist." });
     })
-    .catch(err => res.status(500).json({ success: false, error: "`User` information could not be retrieved." }));
+    .catch(err => res.status(500).json({ success: false, error: "User information could not be retrieved." }));
 });
 
 server.post('/api/users', (req, res) => {
@@ -36,12 +36,39 @@ server.post('/api/users', (req, res) => {
   const { name, bio } = newUser;
 
   if (!name || !bio)
-    res.status(400).json({ success: false, error: "Please provide a `name` and `bio` for new users." });
+    res.status(400).json({ success: false, error: "Please provide a name and bio for new users." });
 
   db
     .insert(newUser)
     .then(user => res.status(201).json({ success: true, user }))
     .catch(err => res.status(500).json({ success: false, error: "There was an error while saving user to database." }));
+});
+
+server.put('/api/users/:id', (req, res) => {
+  const { id } = req.params;
+  const updatedUser = req.body;
+  const { name, bio } = updatedUser;
+
+  if (!name || !bio)
+    res.status(400).json({ success: false, error: "Please provide a name and bio for the user." });
+
+  db
+    .findById(id)
+    .then(user => {
+      if (user) {
+        db
+          .update(id, updatedUser)
+          .then(_ => {
+            db
+              .findById(id)
+              .then(user => res.status(200).json({ success: true, user }));
+          })
+          .catch(err => res.status(500).json({ success: false, error: "User information could not be modified." }));
+      } else {
+        res.status(404).json({ success: false, error: "User does not exist." });
+      }
+    })
+    .catch(err => res.status(500).json({ success: false, error: "User information could not be retrieved." }));
 });
 
 server.listen(port, _ => console.log(`now listening on port ${port}`));
