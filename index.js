c630716c0e258cc5630f336ef1ee5932b8244d31
netrunnerd <41193007+netrunnerd@users.noreply.github.com>
2019-07-24 21:57:50 -0400

    POST /api/users

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ server.get('/api/users', (req, res) => {
   db
     .find()
     .then(users => res.status(200).json({ success: true, users }))
-    .catch(err => res.status(500).json({ success: false, err }));
+    .catch(err => res.status(500).json({ success: false, error: "`Users` information could not be retrieved." }));
 });
 
 server.get('/api/users/:id', (req, res) => {
@@ -26,9 +26,22 @@ server.get('/api/users/:id', (req, res) => {
     .findById(id)
     .then(user => {
       if (user) res.status(200).json({ success: true, user });
-      else res.status(404).json({ success: false, message: `User does not exist.`});
+      else res.status(404).json({ success: false, error: "User does not exist." });
     })
-    .catch(err => res.status(500).json({ success: false, err }));
+    .catch(err => res.status(500).json({ success: false, error: "`User` information could not be retrieved." }));
+});
+
+server.post('/api/users', (req, res) => {
+  const newUser = req.body;
+  const { name, bio } = newUser;
+
+  if (!name || !bio)
+    res.status(400).json({ success: false, error: "Please provide a `name` and `bio` for new users." });
+
+  db
+    .insert(newUser)
+    .then(user => res.status(201).json({ success: true, user }))
+    .catch(err => res.status(500).json({ success: false, error: "There was an error while saving user to database." }));
 });
 
 server.listen(port, _ => console.log(`now listening on port ${port}`));
